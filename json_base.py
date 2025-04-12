36a99c5f7bc7076e6d465a7f062cecd4cf63425c
PyCartographer <146126638+NicklasKristiansen@users.noreply.github.com>
2025-04-12 21:55:53 +0000

    created json base class

--- json_base.py
+++ json_base.py
@@ -1,1 +1,21 @@
 
+
+
+
+
+
+
+
+class JSONBase:
+
+
+    def add_text_box(self, name, text):
+        return {
+            "name": f"{name}",
+            "table": [[{"string": f"{text}"}]]
+        }
+
+
+
+
+
